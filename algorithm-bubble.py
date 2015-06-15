a625be41264a0445752f6f5419aecc903e67ac9a
coding2015 <meganhuang2013@icloud.com>
2015-06-15 16:34:40 +0800

    bubble alogrithm

---
+++ algorithm-bubble.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+'''
+wait to improve:
+	use less codes to implement descending or ascending sort
+'''
+
+def Bubble(a, reverse=False):
+	'''
+bubble algorithm:
+	elements compared with it's next element from the tail to the start
+	during the process, bigger/smaller element is exchanged to a fronter position like bubble. 		
+	time-complex: O(n^2)
+	'''
+	n = len(a)
+	if reverse == False:
+		for i in range(n):
+			exchanged = False
+			for j in range(n-1, i, -1):
+				if a[j] < a[j-1]:
+					temp = a[j]
+					a[j] = a[j-1]
+					a[j-1] = temp
+					exchanged = True
+			if exchanged == False:
+				break
+	else:
+		for i in range(n):
+			exchanged = False
+			for j in range(n-1, i, -1):
+				if a[j] > a[j-1]:
+					temp = a[j]
+					a[j] = a[j-1]
+					a[j-1] = temp
+					exchanged = True
+			if exchanged == False:
+				break
+	
+
+def main():
+	print Bubble.__doc__
+	n = int(raw_input('how many elements?> '))
+	a = []	
+	#for i in n: #error: n is a number, it's not iterable 
+	for i in range(n):
+		a.append(int(raw_input('enter an integer> ')))
+	prompt = '''
+Which order do you want to sort?
+	(A)scending (default)
+	(D)escending	
+Please enter choich> '''
+
+	choice = raw_input(prompt)
+	if choice[0].lower() == 'd':
+		reverse = True
+	else:
+		reverse = False
+	print 'befor sort:'
+	print a
+	Bubble(a,reverse)
+	print 'after sort:'
+	print a
+
+if __name__ == '__main__':
+	main()
