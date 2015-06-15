cd72d069b64a98652ae383b88e7167e291e400e5
coding2015 <meganhuang2013@icloud.com>
2015-06-15 20:22:54 +0800

    marked undone process

--- CorePythonProgramming_2nd/ch09_files/9-06_differ.py
+++ CorePythonProgramming_2nd/ch09_files/9-06_differ.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 
-'''compare two files, if they are different:
-show the different line and column numbers where the first difference occurs'''
+'''
+compare two files, if they are different:
+show the different line and column numbers where the first difference occurs
+'''
 
 def ColumnNumber(s1,s2):
-	for i , c1 in enumerate(s1)
-			if c1 != s2[i]
-				return i+1
-
+	for i , c1 in enumerate(s1):
+		if c1 != s2[i]:
+			break
+	# possible case: s1 in s2
+	pass
 
 
 name1 = raw_input('enter a filename> ')
 name2 = raw_input('enter a filename> ')
-
 f1 = open(name1, 'r')
 f2 = open(name2, 'r')
 
-i = 1
-
-
+#compare lines
+pass
+#compare characters
+pass
 
 f1.close()
 f2.close()
