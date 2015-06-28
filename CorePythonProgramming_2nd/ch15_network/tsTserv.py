ab072b05e53598e9bec5e9a186e4ae289ed11128
coding2015 <meganhuang2013@icloud.com>
2015-06-28 21:08:36 +0800

    socket:reuse port

--- CorePythonProgramming_2nd/ch15_network/tsTserv.py
+++ CorePythonProgramming_2nd/ch15_network/tsTserv.py
@@ -18,6 +18,13 @@ ADDR = (HOST, PORT)
 BUFSIZ = 1024
 
 tcpSerSock = socket()	#创建套接字，socket参数取其默认值
+
+tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)	
+		#在未解除绑定的情况(比如服务器退出而客户端未退出)，重新运行服务器会报错
+		#socket.error: [Errno 98] Address already in use
+		#需在约5分钟内系统才清除绑定，释放端口资源
+		#而调用此句将告诉内核重新使用处于TIME_WAIT状态的套接字，不需等待资源释放
+
 tcpSerSock.bind(ADDR)	#绑定套接字到服务器地址
 tcpSerSock.listen(5)	#允许同时5个连接
 
